249e35c5098b685fe34cd8d1409cca2be7bce456
javiding <jasu.viding@2ns.fi>
2018-03-02 13:25:06 +0200

    asf

--- wsgi.py
+++ wsgi.py
@@ -5,21 +5,21 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    try:
+	try:
 		f = open("/mnt/access.log", "a+")
 		f.write("Hello World! Greetings from "+socket.gethostname()+" @ "+datetime.datetime.now()+"\n")
 		f.close()
-    except:
-    	return "Write failed"
+	except:
+		return "Write failed"
 
-    try:
+	try:
 		with open("/mnt/access.log", "r") as myfile:
 			data = myfile.read()
-    except:
-    	return "Read failed"
+	except:
+		return "Read failed"
 
 	return data
 
 
 if __name__ == "__main__":
-    application.run()
+	application.run()

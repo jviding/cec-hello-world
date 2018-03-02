99cf5c0ad31872f18aa969a921797f33f38f0d1d
javiding <jasu.viding@2ns.fi>
2018-03-02 13:32:05 +0200

    return error

--- wsgi.py
+++ wsgi.py
@@ -10,13 +10,13 @@ def hello():
 		f.write("Hello World! Greetings from "+socket.gethostname()+" @ "+datetime.datetime.now()+"\n")
 		f.close()
 	except Exception as e:
-		return e
+		return str(e)
 
 	try:
 		with open("/mnt/access.log", "r") as myfile:
 			data = myfile.read()
 	except Exception as e:
-		return e
+		return str(e)
 
 	return data
 
